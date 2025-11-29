4a21ecfefdabeb104315f9bc8517986064f8d9d9
rocketmike12 <mg.korop@gmail.com>
2025-11-29 13:40:21 +0200

    modal styles done

--- src/components/AuthModal/AuthModal.jsx
+++ src/components/AuthModal/AuthModal.jsx
@@ -19,28 +19,31 @@ export const AuthModal = function ({ isOpen, closeModal }) {
 							<label htmlFor="username" className={styles["modal__input-label"]}>
 								Username
 							</label>
-							<input type="text" name="username" className={styles["modal__input"]} />
+							<input type="text" name="username" placeholder="Username" className={styles["modal__input"]} />
 						</div>
 
 						<div className={styles["modal__input-wrap"]}>
 							<label htmlFor="email" className={styles["modal__input-label"]}>
 								Email
 							</label>
-							<input type="text" name="email" className={styles["modal__input"]} />
+							<input type="text" name="email" placeholder="Email" className={styles["modal__input"]} />
 						</div>
 
 						<div className={styles["modal__input-wrap"]}>
 							<label htmlFor="password" className={styles["modal__input-label"]}>
 								Password
 							</label>
-							<input type="text" name="password" className={styles["modal__input"]} />
+							<input type="text" name="password" placeholder="Password" className={styles["modal__input"]} />
 						</div>
 
 						<button type="submit" className={styles["modal__btn"]}>
 							Sign up
 						</button>
 
-						<a href="">Already have an account? Log In</a>
+						<p className={styles["modal__login"]}>
+							Already have an account? 
+							<a href="" className={styles["modal__login__link"]}>Log In</a>
+						</p>
 					</form>
 				</div>
 			</div>
