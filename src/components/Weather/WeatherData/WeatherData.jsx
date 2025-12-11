6fa81fd4b7d872c750c4823f2b6190a155218623
rocketmike12 <mg.korop@gmail.com>
2025-12-11 17:48:09 +0200

    basic forecast markup done

--- src/components/Weather/WeatherData/WeatherData.jsx
+++ src/components/Weather/WeatherData/WeatherData.jsx
@@ -3,21 +3,80 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { weatherApi } from "../../../apis/weatherApi";
 
+import { WeatherIcon } from "../WeatherIcon";
+
+import styles from "./WeatherData.module.scss";
+
 export const WeatherData = function ({ place }) {
-	const [forecast, setForecast] = useState(null);
+	const [weather, setWeather] = useState(null);
 
 	const getWeather = async function () {
+		const { data } = await weatherApi.get("/weather", { params: { q: place } });
+		setWeather(data);
+		setDate(new Date());
+	};
+
+	const [forecast, setForecast] = useState(null);
+
+	const getForecast = async function () {
 		const { data } = await weatherApi.get("/forecast", { params: { q: place } });
 		setForecast(data);
 	};
 
 	useEffect(() => {
 		getWeather();
+		getForecast();
 	}, []);
 
 	return (
 		<>
-			<p>{JSON.stringify(forecast)}</p>
+			{weather && (
+				<ul className={styles["weather-data"]}>
+					<li className={styles["weather-data__item"]}>
+						<h3 className="weather-data__title">Feels like</h3>
+						{weather.main.feels_like}
+						<WeatherIcon />
+					</li>
+					<li className={styles["weather-data__item"]}>
+						<h3 className="weather-data__title">Min °C</h3>
+						<p>{weather.main.temp_min}°C</p>
+						<h3 className="weather-data__title">Max °C</h3>
+						<p>{weather.main.temp_max}°C</p>
+					</li>
+					<li className={styles["weather-data__item"]}>
+						<h3 className="weather-data__title">Humidity</h3>
+						<p>{weather.main.humidity}%</p>
+						<WeatherIcon />
+					</li>
+					<li className={styles["weather-data__item"]}>
+						<h3 className="weather-data__title">Pressure</h3>
+						<p>{weather.main.pressure} Pa</p>
+						<WeatherIcon />
+					</li>
+					<li className={styles["weather-data__item"]}>
+						<h3 className="weather-data__title">Wind speed</h3>
+						<p>{weather.wind.speed} m/s</p>
+						<WeatherIcon />
+					</li>
+					<li className={styles["weather-data__item"]}>
+						<h3 className="weather-data__title">Visibility</h3>
+						<p>{weather.visibility}</p>
+						<WeatherIcon />
+					</li>
+				</ul>
+			)}
+
+			{forecast && (
+				<>
+					<div className={styles["hourly"]}>
+						<canvas></canvas>
+					</div>
+
+					<div className={styles["forecast"]}>
+						<ul className={styles["forecast__list"]}></ul>
+					</div>
+				</>
+			)}
 		</>
 	);
 };
--- src/components/Weather/WeatherList/WeatherCard.jsx
+++ src/components/Weather/WeatherList/WeatherCard.jsx
@@ -7,7 +7,7 @@ import { IoReload } from "react-icons/io5";
 import { CiHeart } from "react-icons/ci";
 import { FaRegTrashCan } from "react-icons/fa6";
 
-import { WeatherIcon } from "./WeatherIcon";
+import { WeatherIcon } from "../WeatherIcon";
 
 import styles from "./WeatherCard.module.scss";
 
@@ -33,33 +33,33 @@ export const WeatherCard = function ({ openForecast, closeForecast, place }) {
 	return weather ? (
 		<>
 			<li>
-				<div className="weather__card__location">
-					<p className="weather__card__location__city">{weather.name}</p>
-					<p className="weather__card__location__country"></p>
+				<div className={styles["weather__card__location"]}>
+					<p className={styles["weather__card__location__city"]}>{weather.name}</p>
+					<p className={styles["weather__card__location__country"]}></p>
 				</div>
 
-				<p className="weather__card__time">{`${date.toLocaleTimeString("en-GB", { hour: "numeric" })}:${date.toLocaleTimeString("en-GB", { minute: "numeric" })}`}</p>
+				<p className={styles["weather__card__time"]}>{`${date.toLocaleTimeString("en-GB", { hour: "numeric" })}:${date.toLocaleTimeString("en-GB", { minute: "numeric" })}`}</p>
 
-				<div className="weather__card__date">
-					<p className="weather__card__date__date">{`${date.toLocaleDateString("en-GB", { day: "numeric" })}.${date.toLocaleDateString("en-GB", { month: "numeric" })}.${date.toLocaleDateString("en-GB", { year: "numeric" })} `}</p>
-					<p className="weather__card__date__day">{date.toLocaleDateString("en-GB", { weekday: "long" })}</p>
+				<div className={styles["weather__card__date"]}>
+					<p className={styles["weather__card__date__date"]}>{`${date.toLocaleDateString("en-GB", { day: "numeric" })}.${date.toLocaleDateString("en-GB", { month: "numeric" })}.${date.toLocaleDateString("en-GB", { year: "numeric" })} `}</p>
+					<p className={styles["weather__card__date__day"]}>{date.toLocaleDateString("en-GB", { weekday: "long" })}</p>
 				</div>
 
 				<WeatherIcon icon={weather.weather[0].icon} />
 
-				<p className="weather__card__temperature">{Math.floor(weather.main.temp)}°C</p>
+				<p className={styles["weather__card__temperature"]}>{Math.floor(weather.main.temp)}°C</p>
 
-				<div className="weather__card__button-wrap">
-					<button className="weather__card__update-button">
+				<div className={styles["weather__card__button-wrap"]}>
+					<button className={styles["weather__card__update-button"]}>
 						<IoReload />
 					</button>
-					<button className="weather__card__favorite-button">
+					<button className={styles["weather__card__favorite-button"]}>
 						<CiHeart />
 					</button>
-					<button onClick={handleForecast} className="weather__card__forecast-button">
+					<button onClick={handleForecast} className={styles["weather__card__forecast-button"]}>
 						Forecast
 					</button>
-					<button className="weather__card__delete-button">
+					<button className={styles["weather__card__delete-button"]}>
 						<FaRegTrashCan />
 					</button>
 				</div>
